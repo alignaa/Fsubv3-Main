6eb5ab38757fd1d8fab1a52b7f98d27c9650ab9f
Renz <alightmotionprem229@gmail.com>
2024-11-26 07:03:21 +0700

    up

--- plugins/batch.py
+++ plugins/batch.py
@@ -1,83 +1,63 @@
-import config
+from config import *
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
 from core.bot import Bot
-from core import func
+from core.func import *
 
-
-@Bot.on_message(filters.private & filters.user(config.ADMINS) & filters.command("batch"))
-async def batch(c: Bot, message: Message):
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command("batch"))
+async def batch(client: Client, message: Message):
     while True:
         try:
-            # Meminta pesan pertama atau tautan
-            first_message = await c.ask(
-                text="Teruskan pesan pertama atau paste link post dari CHANNEL_DB.",
+            first_message = await client.ask(
+                text="<b>Silahkan Forward Pesan/File Pertama dari Channel DataBase. (Forward with Qoute)</b>\n\n<b>atau Kirim Link Postingan dari Channel Database</b>",
                 chat_id=message.from_user.id,
                 filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
                 timeout=60,
             )
-        except Exception:
-            await message.reply("Waktu habis. Silakan coba lagi.", quote=True)
+        except BaseException:
             return
-
-        # Mendapatkan ID pesan pertama
-        f_msg_id = await func.get_message_id(c, first_message)
-        if f_msg_id is not None:
+        f_msg_id = await get_message_id(client, first_message)
+        if f_msg_id:
             break
-
         await first_message.reply(
-            "Pesan tidak valid atau tidak berasal dari channel yang sesuai.",
+            "❌ <b>ERROR</b>\n\n<b>Postingan yang Diforward ini bukan dari Channel Database saya</b>",
             quote=True,
         )
         continue
 
     while True:
         try:
-            # Meminta pesan terakhir atau tautan
-            second_message = await c.ask(
-                text="Teruskan pesan akhir atau paste link post dari CHANNEL_DB.",
+            second_message = await client.ask(
+                text="<b>Silahkan Forward Pesan/File Terakhir dari Channel DataBase. (Forward with Qoute)</b>\n\n<b>atau Kirim Link Postingan dari Channel Database</b>",
                 chat_id=message.from_user.id,
                 filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
                 timeout=60,
             )
-        except Exception:
-            await message.reply("Waktu habis. Silakan coba lagi.", quote=True)
+        except BaseException:
             return
-
-        # Mendapatkan ID pesan terakhir
-        s_msg_id = await func.get_message_id(c, second_message)
-        if s_msg_id is not None:
+        s_msg_id = await get_message_id(client, second_message)
+        if s_msg_id:
             break
-
         await second_message.reply(
-            "Pesan tidak valid atau tidak berasal dari channel yang sesuai.",
+            "❌ <b>ERROR</b>\n\n<b>Postingan yang Diforward ini bukan dari Channel Database saya</b>",
             quote=True,
         )
         continue
 
-    # Membuat string unik untuk start parameter
-    try:
-        string = f"get-{f_msg_id * abs(c.db_channel.id)}-{s_msg_id * abs(c.db_channel.id)}"
-        base64_string = await func.encode(string)
-        link = f"https://t.me/{c.username}?start={base64_string}"
-
-        # Membuat tombol untuk membagikan link
-        reply_markup = InlineKeyboardMarkup(
+    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
+    base64_string = await encode(string)
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup(
+        [
             [
-                [
-                    InlineKeyboardButton(
-                        "Bagikan Link", url=f"https://telegram.me/share/url?url={link}"
-                    )
-                ]
+                InlineKeyboardButton(
+                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                )
             ]
-        )
-
-        # Mengirimkan link ke pengguna
-        await second_message.reply_text(
-            f"Berikut adalah link Anda: {link}",
-            quote=True,
-            reply_markup=reply_markup,
-        )
-    except Exception as e:
-        await message.reply(f"Terjadi kesalahan: {e}", quote=True)
+        ]
+    )
+    await second_message.reply_text(
+        f"<b>Link Sharing File Berhasil Di Buat:</b>\n\n{link}",
+        quote=True,
+        reply_markup=reply_markup,
+    )
